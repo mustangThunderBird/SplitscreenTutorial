fd3c83404a5e8de5159b79dca16984ac26015c73
Nicholas Facciola <49330912+mustangThunderBird@users.noreply.github.com>
2021-01-22 12:23:24 -0700

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public enum MovementType
+    {
+        keyboard,
+        controller
+    };
+    public MovementType movementType;
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    float x;
+    float z;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isOnGround;
+
+    private void Update()
+    {
+        if (movementType == MovementType.keyboard)
+        {
+            isOnGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (isOnGround && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            //keyboardmovement
+            x = Input.GetAxis("Horizontal");
+            z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            if (Input.GetButtonDown("Jump") && isOnGround)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+        }
+        else
+        {
+            if(movementType == MovementType.controller)
+            {
+                isOnGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+                if (isOnGround && velocity.y < 0)
+                {
+                    velocity.y = -2f;
+                }
+
+                //keyboardmovement
+                x = Input.GetAxis("HorizontalGamepad");
+                z = Input.GetAxis("VerticalGamepad");
+
+                Vector3 move = transform.right * x + transform.forward * z;
+
+                controller.Move(move * speed * Time.deltaTime);
+
+                if (Input.GetButtonDown("JumpGamepad") && isOnGround)
+                {
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                }
+
+                velocity.y += gravity * Time.deltaTime;
+
+                controller.Move(velocity * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ PlayerLook.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public enum LookType
+    {
+        keyboard,
+        controller
+    };
+    public LookType lookType;
+    public float mouseSensitivity = 245f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    public Camera cam;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if(lookType == LookType.keyboard)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f).normalized;
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+        else
+        {
+            if(lookType == LookType.controller)
+            {
+                float mouseX = Input.GetAxis("Gamepad X") * mouseSensitivity * Time.deltaTime;
+                float mouseY = Input.GetAxis("Gamepad Y") * mouseSensitivity * Time.deltaTime;
+
+                xRotation -= mouseY;
+                xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+                transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f).normalized;
+                playerBody.Rotate(Vector3.up * mouseX);
+            }
+        }
+    }
+}
